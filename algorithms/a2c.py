8e4229e4bddf9044f59c9a02350d8b9342a3ad4b
sebastianthomas <sebthomas.4199@gmail.com>
2025-03-05 23:35:50 -0800

    changing rlcard-setup.py to rlcard_setup.py

---
+++ algorithms/a2c.py
@@ -0,0 +1,12 @@
+from env.rlcard_setup import RLCardEnv
+from logs.logger import Logger
+
+"""
+Class implementing the Advantage Actor Critic algorithm.
+"""
+
+class A2C:
+    def __init__(self):
+        self.env = RLCardEnv()
+        self.log = Logger(log_name="A2C-Algorithm")
+        self.log.info("Initializing A2C Algorithm.")
